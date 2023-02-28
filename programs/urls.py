cf5cc90bff261d37feeed17d42d9261344a1322e
Ewie1 <shephvo12@gmail.com>
2023-02-28 22:05:33 +0000

    Create view, template and connect url for program details display

--- programs/urls.py
+++ programs/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_all_programs_page, name='programs'),
+    path('show_program/<program_id>', views.SingleProgram.as_view(), name='show_program')
 ]
