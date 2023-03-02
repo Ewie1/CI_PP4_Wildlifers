592e5031da0190d5fc86956e1c8d204964752803
Ewie1 <shephvo12@gmail.com>
2023-03-02 19:45:49 +0000

    Create view to render booking form

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic, View
+from django.contrib.auth.models import User
+
+from .models import ProgramBooking, ProgramCheckIn
+from .forms import ProgramBookingForm, ProgramCheckInForm
+
+
+class Booking(View):
+    def post(self, request):
+        program_booking_form = ProgramBookingForm(data=request.POST)
+        
+        if program_booking_form.is_valid():
+            reservation = program_booking_form.save(commit=True)
+            reservation.save()
+            messages.success(request, 'Your are booked in!')
+    context = {
+        'program_booking_form': program_booking_form
+        }
+
+    return render(request, '', context)
+
