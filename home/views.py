4a40c6985ce6bd50a5b821f0f0c9ce11b8a326fa
Ewie1 <shephvo12@gmail.com>
2023-03-07 16:15:58 +0000

    Write view code and connect url to display contact form

--- home/views.py
+++ home/views.py
@@ -8,3 +8,17 @@ def home(request):
     View to render home page
     """
     return render(request, 'home/index.html')
+
+
+def contact(request):
+    """
+    Function to dispaly contact form
+    """
+
+    if request.method == 'POST':
+        your_name = request.POST['your_name']
+        your_email = request.POST['your_email']
+        your_message = request.POST['tour_message']
+        return render(request, 'home/contact.html', {})
+    else:
+        return render(request, 'home/contact.html', {})
