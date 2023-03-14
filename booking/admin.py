5fd331fb8b639aac1c4e0367345b13e1416914b2
Ewie1 <shephvo12@gmail.com>
2023-03-14 02:31:49 +0000

    Add list display and list filter to EnrollAdmin model

--- booking/admin.py
+++ booking/admin.py
@@ -9,3 +9,27 @@ from booking.models import Enroll
 
 
 admin.site.register(Enroll)
+
+@admin.register(Enroll)
+class EnrollAdmin(admin.ModelAdmin):
+    list_filter = (
+        'user',
+        'start_date',
+        'email',
+        'animal_name',
+        'volunteer_job',
+        'work_time',
+        'created_on'
+    )
+
+    list_display = (
+        'user',
+        'start_date',
+        'email',
+        'animal_name',
+        'volunteer_job',
+        'work_time',
+        'created_on'
+    )
+
+     
