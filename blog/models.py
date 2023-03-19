239e72ff1f6d49a6845ed1ff65e8c194f3037629
Ewie1 <shephvo12@gmail.com>
2023-03-19 19:11:09 +0000

    Add style to contact page and programs page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'DRAFT'), (1, 'POST'))
+
+
+class Post(models.Model):
+    """
+    Class for site blog
+    """
+    title = models.CharField(max_length=255)
+    
