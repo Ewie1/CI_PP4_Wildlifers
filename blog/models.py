9a893a6f84bc34b5d105ccca9e72709ba2729d41
Ewie1 <shephvo12@gmail.com>
2023-03-20 12:55:50 +0000

    Create Post and Comment class for blog model

--- blog/models.py
+++ blog/models.py
@@ -10,4 +10,47 @@ class Post(models.Model):
     Class for site blog
     """
     title = models.CharField(max_length=255)
-    
+    slug = models.SlugField(max_length=200, unique=True)
+    post_id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+        )
+    create_date = models.DateTimeField(blank=True)
+    update = models.DateTimeField()
+    content = models.TextField()
+    featured_image = CloudinaryField(
+        'image',
+        default='placeholder'
+        )
+    excert = models.TextField(blank=True)
+    status = models.IntegerField(choices='STATUS', default=0)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
+
+    def like_count(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comment'
+        )
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    create_date = models.DateTimeField(blank=True)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return f'Comment {self.body} by {self.name}'
