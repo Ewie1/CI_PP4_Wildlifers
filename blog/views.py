5c85ddd052f69c0eba79900ab2011c2c2447608e
Ewie1 <shephvo12@gmail.com>
2023-03-21 15:11:17 +0000

    Add forms file for comment, fix typo for blog view and model file

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ from django.views import generic
 from .models import Post
 
 
-class PostDisplay(generic.View):
+class PostDisplay(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_date")
-    template_name = "blog.html"
+    template_name = 'blog/blog_list.html'
     paginate_by = 4
+
--- programs/views.py
+++ programs/views.py
@@ -10,29 +10,6 @@ from django.contrib import messages
 from .models import Program
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#from .filters import ProgramFilter
-
-
-#def show_all_programs_page(request):
-#    context = {}
-
-#    filtered_programs = ProgramFilter(
-#        request.GET,
-#        queryset=Program.objects.all()
-#    )
-
-#    context['filtered_programs'] = filtered_programs
-
- #   paginated_filtered_programs = Paginator(filtered_programs.qs, 1)
-
-#    page_number = request.GET.get('page')
-#    program_page_obj = paginated_filtered_programs.get_page(page_number)
-
-#    context['program_page_obj'] = program_page_obj
-#    context['all_programs'] = Program.objects.all()
-
-#   return render(request, 'programs/programs_list.html', context)
-
 
 def show_all_programs_page(request):
     """
