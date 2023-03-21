b734e83d4ea48ba0cbc7e9b94060e4ff35207e28
Ewie1 <shephvo12@gmail.com>
2023-03-21 16:30:37 +0000

    Add function and fix script.js file path to control message timeout

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+setTimeout(function () {
+    let messages = document.getElementById('msg');
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+  }, 1500);
+
+setTimeout()
+
