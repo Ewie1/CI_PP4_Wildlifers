340739698d39b1414b13a5624ff66faa67709f4c
Ewie1 <shephvo12@gmail.com>
2023-03-23 23:35:15 +0000

    Style program page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.PostDisplay.as_view(), name='blog'),
-    path('<slug:slug>', views.ReadPost.as_view(), name='read_post')
+   # path('blog/', views.PostDisplay.as_view(), name='blog'),
+   # path('<slug:slug>', views.ReadPost.as_view(), name='read_post')
 ]
