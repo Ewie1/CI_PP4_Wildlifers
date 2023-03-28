fd12f8b099c60542e9757a60f0b035d02bcd0695
Ewie1 <shephvo12@gmail.com>
2023-03-28 18:37:11 +0000

    Add python code validation images

--- wildlifers/urls.py
+++ wildlifers/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
+# Project app urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
